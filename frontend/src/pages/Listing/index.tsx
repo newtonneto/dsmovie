4bd9ec5a30c30321ba166dd635cfcdca0b9e5399
newtonneto <newton.mgs@gmail.com>
2022-01-14 10:16:45 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -28,10 +28,14 @@ function Listing() {
         setPage(data);
       });
   }, [pageNumber]);
+
+  const handlePageChange = (newPageNumber : number) => {
+    setPageNumber(newPageNumber);
+  }
   
   return (
     <>
-      <Pagination />
+      <Pagination page={page} onChange={handlePageChange} />
 
       <div className="container">
         <div className="row">
