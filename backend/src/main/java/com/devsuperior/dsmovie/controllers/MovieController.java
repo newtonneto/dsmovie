821b09420fa8d5d49379ced55b8c730b53e8e10f
joao.gomes <vitorjoaofg@gmail.com>
2022-04-16 10:56:45 -0300

    endpoints for testing

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
@@ -27,4 +27,14 @@ public class MovieController {
 	public MovieDTO findById(@PathVariable Long id) {
 		return service.findById(id);
 	}
+
+	@GetMapping(value = "/functionone")
+	public String functionone() {
+		return "(setData, setLoading) => { let info = ['']; const counts = {}; const structuredData = [{}]; info = dataSource.map((item, index) => { return item.stage; }); info.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; }); for (var key in counts) { if (counts.hasOwnProperty(key)) { structuredData.push({ arg: key, val: counts[key] }); } } setData(structuredData); setLoading(false); }";
+	}
+
+	@GetMapping(value = "/functiontwo")
+	public String functiotwo() {
+		return "(setData, setLoading) => { let info = ['']; const counts = {}; const structuredData = [{}]; info = dataSource.map((item, index) => { return item.absenceType.name; }); info.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; }); for (var key in counts) { if (counts.hasOwnProperty(key)) { structuredData.push({ arg: key, val: counts[key] }); } } setData(structuredData); setLoading(false); }";
+	}
 }
