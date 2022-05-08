d940027b38a7a1d0d2b1ed9de0e96352e18f27e5
joao.gomes <vitorjoaofg@gmail.com>
2022-05-08 20:57:18 -0300

    new function

--- backend/src/main/resources/static/classifyAbsence.js
+++ backend/src/main/resources/static/classifyAbsence.js
@@ -1,18 +1,21 @@
-function classify(data, setData, setLoading, series) {
+const classify = function (data, setData, setLoading, series) {
   let info = [""];
   const counts = {};
   const structuredData = [{}];
   info = data.map((item, index) => {
-    return item.absenceType.name;
+    return item.stage;
   });
   info.forEach(function (x) {
     counts[x] = (counts[x] || 0) + 1;
   });
   for (var key in counts) {
     if (counts.hasOwnProperty(key)) {
-      structuredData.push({ [series.argumentField]: key, [series.valueField]: counts[key] });
+      structuredData.push({
+        [series.argumentField]: key,
+        [series.valueField]: counts[key],
+      });
     }
   }
   setData(structuredData);
   setLoading(false);
-}
+};
